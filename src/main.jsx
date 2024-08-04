618d9478c21e6e4fe5d90883b421532c648e666e
Murali krishna <murali.yernena@careers360.com>
2024-08-05 02:43:05 +0530

    fix: error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import installationPrompt from './utils/installationPrompt.js'
+import installationPrompt from './utils/installationPrompt.jsx'
 
 installationPrompt()
 
