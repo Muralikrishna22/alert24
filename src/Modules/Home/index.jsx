4c52ce35bbecfe9da759b65e507b8749df2895c0
Murali krishna <murali.yernena@careers360.com>
2024-08-05 23:32:39 +0530

    home page initiated

---
+++ src/Modules/Home/index.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import './styles.css'
+import { FaCloudMoonRain } from "react-icons/fa";
+import { RiWindyLine } from "react-icons/ri";
+import { WiHumidity } from "react-icons/wi";
+import { TbAirConditioning } from "react-icons/tb";
+import { IoHomeSharp } from "react-icons/io5";
+import { IoIosPeople } from "react-icons/io";
+import { FaHandsHelping } from "react-icons/fa";
+import { BiSolidBellRing } from "react-icons/bi";
+
+
+
+const Home = () => {
+    function showSystemNotification() {
+        if ("Notification" in window) {
+          if (Notification.permission === "granted") {
+            let notification = new Notification("Need your attention", {
+              body: "This site uses notifications for the best user experience. Thank you for understanding",
+              icon: 'ALERT 24',
+            });
+          } else if (Notification.permission !== "denied") {
+            Notification.requestPermission().then((permission) => {
+              if (permission === "granted") {
+                let notification = new Notification("Need your attention", {
+                  body: "This site uses notifications for the best user experience. Thank you for understanding",
+                  icon: 'ALERT 24',
+                });
+              } else if (permission === "denied") {
+                alert("This site uses notifications for the best user experience. Thank you for understanding");
+              }
+            });
+          }
+        }
+      }
+
+    return (
+        <div className='home_container'>
+            <div className='banner_container'>
+                Cloudy Weather out side most chances of rain.
+            </div>
+            <div className='sub_menu'>
+                <div className='item'>
+                    <FaCloudMoonRain className="icon" />
+                    <div>
+                        Rainy
+                    </div>
+                </div>
+                <div className='item'>
+                    <RiWindyLine className="icon" />
+                    <div>
+                        Windy
+                    </div>
+                </div>
+                <div className='item'>
+                    <WiHumidity className="icon" />
+                    <div>
+                        Humid
+                    </div>
+                </div>
+                <div className='item'>
+                    <TbAirConditioning className="icon" />
+                    <div>
+                        Air Quality
+                    </div>
+                </div>
+            </div>
+            <div className='categories_list'>
+                <div className='category_item'>
+                    <IoHomeSharp className="icon" />
+                    <div>
+                        Address Registration
+                    </div>
+                </div>
+                <div className='category_item'>
+                    <IoIosPeople className="icon" />
+
+                    <div>
+                        Manage connections
+
+                    </div>
+                </div>
+                <div className='category_item' onClick={showSystemNotification}>
+                    <BiSolidBellRing className="icon" />
+                    <div>
+                        Alerts
+
+                    </div>
+                </div>
+                <div className='category_item'>
+                    <FaHandsHelping className="icon" />
+                    <div>
+                        Help/Complants
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
