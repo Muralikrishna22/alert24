30cd8399f53b565111c43535ec47373cce864499
Murali krishna <murali.yernena@careers360.com>
2024-08-21 00:00:58 +0530

    fire bassse integration WIP

--- src/Common/Base/TextInput/index.jsx
+++ src/Common/Base/TextInput/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import './styles.css';
 
-const TextInput = ({placeholder, helpText=''}) => (
+const TextInput = ({placeholder, helpText='', ...rest}) => (
     <div className={`${helpText && 'help=text'}`}>
-    <input type="text" placeholder={placeholder} className='input' />
+    <input type="text" placeholder={placeholder} className='input' {...rest}/>
     {helpText && (<span class="help-block">{helpText}</span>)}
     </div>
 );
--- src/Modules/LoginPage/index.jsx
+++ src/Modules/LoginPage/index.jsx
@@ -1,29 +1,120 @@
-import React, { useState } from 'react';
-import './styles.css';
-import Base from '../../Common/Base';
-import TextInput from '../../Common/Base/TextInput';
-import Button from '../../Common/Base/Button';
+import React, { useState } from "react";
+import "./styles.css";
+import Base from "../../Common/Base";
+import TextInput from "../../Common/Base/TextInput";
+import Button from "../../Common/Base/Button";
 import { FaUser } from "react-icons/fa6";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { firebase, auth } from "../../firebase";
 
 // Registration Form Component
 const LoginForm = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+  const [mynumber, setnumber] = useState("+916303582626");
+  const [otp, setotp] = useState("");
+  const [show, setshow] = useState(false);
+  const [final, setfinal] = useState("");
+
+  // Sent OTP
+  const signin = () => {
+    if (mynumber === "" || mynumber.length < 10) return;
+
+
+    let verify = new firebase.auth.RecaptchaVerifier("recaptcha-container");
+    auth
+      .signInWithPhoneNumber(mynumber, verify)
+      .then((result) => {
+        setfinal(result);
+        alert("code sent");
+        setshow(true);
+      })
+      .catch((err) => {
+        alert(err);
+        window.location.reload();
+      });
+  };
+
+  // Validate OTP
+  const ValidateOtp = () => {
+    if (otp === null || final === null) return;
+    final
+      .confirm(otp)
+      .then((result) => {
+        // success
+      })
+      .catch((err) => {
+        alert("Wrong code");
+      });
+  };
+
   return (
-    <div className='container'>
-      <div className='formContainer'>
-        <FaUser className='avatar' />
-        <form className='form'>
-          <TextInput placeholder="Contact No" />
+    <div style={{ marginTop: "200px" }}>
+        <center>
+            <div
+                style={{
+                    display: !show ? "block" : "none",
+                }}
+            >
+                <input
+                    value={mynumber}
+                    onChange={(e) => {
+                        setnumber(e.target.value);
+                    }}
+                    placeholder="phone number"
+                />
+                <br />
+                <br />
+                <div id="recaptcha-container"></div>
+                <br />
+                <button onClick={signin}>
+                    Send OTP
+                </button>
+            </div>
+            <div
+                style={{
+                    display: show ? "block" : "none",
+                }}
+            >
+                <input
+                    type="text"
+                    placeholder={"Enter your OTP"}
+                    onChange={(e) => {
+                        setotp(e.target.value);
+                    }}
+                ></input>
+                <br />
+                <br />
+                <button onClick={ValidateOtp}>
+                    Verify
+                </button>
+            </div>
+        </center>
+    </div>
+);
+
+  return (
+    <div className="container">
+      <div className="formContainer">
+        <FaUser className="avatar" />
+        <form className="form">
+          <TextInput placeholder="Contact No" onChange={(e) => {console.log(".....", e.target.value)}} />
           <TextInput placeholder="Email (Optional)" />
-          <Button isDisabled={isLoggedIn} onClick={() => setIsLoggedIn(prev => !prev)} type="button">Login</Button>
+          <Button
+            isDisabled={isLoggedIn}
+            onClick={() => setIsLoggedIn((prev) => !prev)}
+            type="button"
+          >
+            Login
+          </Button>
         </form>
         <div>
           {isLoggedIn && (
             <>
-            <TextInput placeholder="Enter OTP... " />
-            <Button onClick={() => navigate('/home')} type="button">Verify</Button>
+              <TextInput placeholder="Enter OTP... " />
+              <Button onClick={() => navigate("/home")} type="button">
+                Verify
+              </Button>
             </>
           )}
         </div>
